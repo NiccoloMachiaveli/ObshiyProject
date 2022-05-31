f5be1042e791d684568c882f41238e1cfdee183d
NiccoloMachiaveli <106552508+NiccoloMachiaveli@users.noreply.github.com>
2022-06-01 00:21:07 +0400

    Add files via upload

---
+++ GitTestFile/GitTestFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTestFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
